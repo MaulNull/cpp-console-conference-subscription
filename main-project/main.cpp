ef6e9443c6e26b1a3d084829af75a7d6533f9b02
MaulNull <gomail1138@gmail.com>
2025-05-27 22:53:51 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #4. GIT\n";
+    cout << "Variant #2. Conference Subscription\n";
+    cout << "Author: Vladislav Fedotov\n";
     return 0;
 }
