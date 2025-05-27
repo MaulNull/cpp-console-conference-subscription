07f92b8d63250fb74618e5a1b50a6985ac0e5605
MaulNull <gomail1138@gmail.com>
2025-05-28 00:04:04 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #4. GIT\n";
     cout << "Variant #2. Conference Subscription\n";
     cout << "Author: Vladislav Fedotov\n";
+    cout << "Group: 24\n";
     return 0;
 }
