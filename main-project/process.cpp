984c38de184f9fa13cbd778b9c4375789ee124f7
MaulNull <gomail1138@gmail.com>
2025-05-28 23:57:12 +0300

    Добавлена реализация функции обработки

--- main-project/process.cpp
+++ main-project/process.cpp
@@ -1,7 +1,22 @@
 #include "processing.h"
 
+int diff(period a, period b)
+{
+	int x = a.hours * 60 + a.minutes;
+	int y = b.hours * 60 + b.minutes;
+	return (x > y ? x - y : y - x);
+}
+
 int process(lecture_subscription* array[], int size)
 {
-	
-	return 0;
+	int max = diff(array[0]->start, array[0]->finish);
+	for (int i = 1; i < size; i++)
+	{
+		int curr = diff(array[i]->start, array[i]->finish);
+		if (curr > max)
+		{
+			max = curr;
+		}
+	}
+	return max;
 }
